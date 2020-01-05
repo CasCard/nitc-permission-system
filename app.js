3f3f2c85a1cb63a46f97a1d0f9d9ec8672aa4db5
abel <abelcheruvathoor@gmail.com>
2020-01-05 21:05:12 +0530

    updated dependenceis and PORT

--- app.js
+++ app.js
@@ -245,7 +245,8 @@ app.get("/logout", function(req, res) {
 });
 
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, function() {
+  console.log("Server started on port ${PORT}");
 });
